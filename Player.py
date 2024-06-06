683d3d31f8c1a53ecbff2dac3c9a1c8db24c04c6
Olivér Ősz <16599031+oliverosz@users.noreply.github.com>
2024-06-06 12:33:45 +0200

    Refactor Game

--- Player.py
+++ Player.py
@@ -2,8 +2,8 @@ from Inventory import Inventory
 
 class Player:
     # A játékosokhoz leíró class, tartalmazza a személyes scorejukat, színüket, inventoriukat, opcionálisan a board pozíciójukat
-    def __init__(self, color, Inventory, player_name, score=0):
-        self.player_name = player_name
+    def __init__(self, color, player_name, score=0):
+        self.name = player_name
         self.color = color  # Játékos színe
         self.inventory = Inventory()  # Inventory initializálása
         self.score = score  # A játékos score-ja, vagyis hogy mennyi token küldetést teljesített, ha eljut 10 ig a játékos nyer
